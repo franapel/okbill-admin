3e76b5eb7ea6549af3e8d87fbc3fdfe30bf38c21
franapel <perezlefiman2@gmail.com>
2021-10-21 17:39:19 -0300

    Agregar slider del estado de las mesas v1

--- src/components/HistoryList.js
+++ src/components/HistoryList.js
@@ -1,7 +1,20 @@
 import * as React from "react";
 import { List, useListContext, TextField, ReferenceField, ReferenceInput, Create, Edit, SimpleForm, TextInput, required, DateTimeInput, NullableBooleanInput, SelectInput, useGetList } from 'react-admin';
-import { List as MUIList, ListItem, ListItemText, ImageList, ImageListItem, ListItemIcon } from '@mui/material';
+import { List as MUIList, ListItem, ListItemText, ImageList, ImageListItem, Paper, ClickAwayListener, Slide, Box } from '@mui/material';
 import { useHistory } from "react-router"
+import { useTheme } from '@mui/material/styles';
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+
+const emptyTableEvent = {
+    id: "",
+    table_id: "",
+    user_id: "",
+    state: "",
+    payed: "",
+    date: ""
+}
 
 const tableStates = [
     {
@@ -55,26 +68,22 @@ function getTimeAndMid(dateString) {
 }
 
 function getLatestTableEvent(table_id, history) {
-    if (!history) return {}
+    if (!history) return emptyTableEvent
     const tableHistory = history.filter(tableEvent => tableEvent.table_id === table_id)
     let lastEvent = tableHistory[0]
     tableHistory.forEach(tableEvent => {
         if (lastEvent.date < tableEvent.date) lastEvent = tableEvent
     })
-    return lastEvent
-}
-function getLatestTableState(table_id, history) {
-    const tableEvent = getLatestTableEvent(table_id, history)
-    if (tableEvent) return tableEvent.state
-    else return "none"
+    if (lastEvent) return lastEvent
+    else return emptyTableEvent
 }
 
 const EventGrid = () => {
     const { ids, data } = useListContext();
     const history = useHistory()
-    return (
-        <MUIList style={{ minHeight: 500, maxHeight: 500, overflowY: "scroll" }} disablePadding>
 
+    return (
+        <MUIList disablePadding sx={{ height: "70vh", overflowY: "auto" }}>
             {ids.map(id =>
                 <ListItem key={id} sx={{ p: 0, m: 0, height: 75, borderBottom: 1, borderColor: "grey.300", "&:hover": { backgroundColor: "grey.300" } }} onClick={e => {
                     e.stopPropagation()
@@ -87,33 +96,80 @@ const EventGrid = () => {
                         </ReferenceField>
                     } sx={{ width: "60%", height: 1, borderLeft: 1, borderColor: "grey.300" }}>
                     </ListItemText>
-                    <ListItemIcon sx={{ width: "20%" }}>
-                        <img src={getTableImageSrc(data[id].state)} style={{ maxWidth: "100%" }} alt="Mesa" />
-                    </ListItemIcon>
+                    <img src={getTableImageSrc(data[id].state)} style={{ height: "80%" }} alt="Mesa" />
                 </ListItem>
             )}
         </MUIList>
+
     );
 };
 
 const TablesMap = () => {
-    let { ids } = useGetList('tables');
-    const hData = useGetList("history").data
-    const hIds = useGetList("history").ids
+    const { ids } = useGetList('tables');
+    const hData = useListContext().data
+    const hIds = useListContext().ids
     const history = hIds.map(id => hData[id])
+
+    const [openTableDetail, setOpenTableDetail] = React.useState(false)
+    const [activatedTableClick, setActivatedTableClick] = React.useState(true)
+    const [tableEventToShow, setTableEventToShow] = React.useState()
+
+    let timeout
+    function handleClick(tableId) {
+        if (activatedTableClick && !openTableDetail) {
+            setTableEventToShow(getLatestTableEvent(tableId, history))
+            setOpenTableDetail(true)
+            setActivatedTableClick(false)
+        }
+    }
+    function handleClickAway() {
+        if (openTableDetail) {
+            if (timeout !== undefined) clearTimeout(timeout)
+            timeout = setTimeout(() => {
+                setOpenTableDetail(false)
+                setActivatedTableClick(true)
+            }, 50)
+        }
+    }
+
     return (
-        <ImageList sx={{ width: "50%", padding: '2em 6em' }} cols={4}>
-            {ids.map(id =>
-                <ImageListItem key={id}  >
-                    <img src={getTableImageSrc(getLatestTableState(id, history))} alt="Mesa" />
-                    <div style={{ position: "absolute", left: "50%", bottom: "50%", transform: "translate(-50%, 0)" }}>
-                        {id}
-                    </div>
-                </ImageListItem>
-            ).reverse()
-            }
-        </ImageList>
+        <>
+            <ImageList sx={{ width: "60%", height: "fit-content" }} cols={4}>
+                {ids.map(id =>
+                    <ImageListItem key={id}>
+                        <img src={getTableImageSrc(getLatestTableEvent(id, history).state)} alt="Mesa" onClick={() => handleClick(id)} />
+                        <div style={{ position: "absolute", left: "50%", bottom: "50%", transform: "translate(-50%, 0)" }}>
+                            {id}
+                        </div>
+                    </ImageListItem>
+                ).reverse()
+                }
+            </ImageList>
+            {openTableDetail && <ClickAwayListener onClickAway={handleClickAway}>
+                <Slide direction="left" in={openTableDetail} mountOnEnter unmountOnExit>
+                    <Paper elevation={4} sx={{ position: "absolute", top: 60, right: 0, height: "90vh", width: 500 }}>
+                        <p>ID tabla: <TextField record={tableEventToShow} source="table_id" /></p>
+                        <p>Usuario: <ReferenceField record={tableEventToShow} source="user_id" reference="users">
+                                <TextField source="name" />
+                            </ReferenceField>
+                        </p>
+                        <p>Estado: <TextField record={tableEventToShow} source="state" /></p>
+                        <p>Fecha: <TextField record={tableEventToShow} source="date" /></p>
+                        <p>Pagado: <TextField record={tableEventToShow} source="payed" /></p>
+                    </Paper>
+                </Slide>
+            </ClickAwayListener>}
+        </>
     );
+}
+const AsideTablesMap = () => {
+    const theme = useTheme();
+    const matches = useMediaQuery(theme.breakpoints.up('sm'));
+    return (matches &&
+        <Box sx={{ width: "70%", display: "flex", justifyContent: "center" }}>
+            <TablesMap />
+        </Box>
+    )
 }
 
 const historyFilters = [
@@ -127,7 +183,7 @@ const historyFilters = [
 ]
 
 export const EventList = (props) => (
-    <List filters={historyFilters} aside={<TablesMap />} pagination={null} {...props}>
+    <List filters={historyFilters} aside={<AsideTablesMap />} pagination={null} {...props}>
         <EventGrid />
     </List>
 );
@@ -135,7 +191,7 @@ export const EventList = (props) => (
 export const EventCreate = (props) => (
     <Create {...props}>
         <SimpleForm>
-            <TextInput disabled label="ID" source="id" initialValue="0"/>
+            <TextInput disabled label="ID" source="id" initialValue="0" />
             <TextInput source="table_id" label="ID de la mesa" />
             <TextInput source="user_id" label="ID del usuario" validate={required()} />
             <SelectInput source="state" choices={tableStates} />
